f5bd210314461df154662ec5069f7ff4b8033465
Ben Ohana <ben.ohana@sap.com>
2025-03-03 11:13:30 +0200

    create minimal server

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+
+const server = Fastify({
+    logger: {
+        transport: {
+            target: "pino-pretty"
+        },
+    }
+});
+
+server.register(fastifyStatic, {
+    root: path.join(__dirname, 'client', 'dist')
+});
+
+server.get('/', async (request, reply) => {
+    return { success: true, message: 'Hello World!!' };
+});
+
+try {
+    await server.listen({ port: 3000 });
+    server.log.info(`Server listening on 3000`);
+} catch (error) {
+    server.log.error(error);    
+    process.exit(1);
+}
+
+if (import.meta.hot) {
+    import.meta.hot.on("vite:beforeFullReload", () => {
+        server.log.warn('Server is restarting...');
+        server.close();
+    });
+}
